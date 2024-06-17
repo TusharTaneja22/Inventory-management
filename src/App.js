b6cbf549247b92d621340ae432875d3ff4a4940c
Tushar Taneja <tushartaneja@Tushars-MacBook-Air.local>
2024-06-17 15:33:28 +0530

    minor_change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
-import Header from './components/Header';
-import InventoryStats from './components/InventoryStats';
-import ProductTable from './components/ProductTable';
-import { setProducts, setStats } from './actions/actions';
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import Header from "./components/Header";
+import InventoryStats from "./components/InventoryStats";
+import ProductTable from "./components/ProductTable";
+import { setProducts, setStats } from "./actions/actions";
+import "./App.css";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -18,30 +18,42 @@ const App = () => {
 
   const fetchProducts = async () => {
     try {
-      const response = await axios.get('https://dev-0tf0hinghgjl39z.api.raw-labs.com/inventory');
+      const response = await axios.get(
+        "https://dev-0tf0hinghgjl39z.api.raw-labs.com/inventory"
+      );
       const data = response.data;
       dispatch(setProducts(data));
       updateStats(data);
     } catch (error) {
-      console.error('Error fetching the data', error);
+      console.error("Error fetching the data", error);
     }
   };
 
-  const updateStats = (updatedProducts) => {
-    const totalProducts = updatedProducts.length;
-    const totalValue = updatedProducts.reduce((acc, product) => acc + parseFloat(product.value.replace('$', '')), 0);
-    const outOfStock = updatedProducts.filter((product) => product.quantity === 0).length;
-    const categories = new Set(updatedProducts.map((product) => product.category)).size;
+  const updateStats = useCallback(
+    (updatedProducts) => {
+      const totalProducts = updatedProducts.length;
+      const totalValue = updatedProducts.reduce(
+        (acc, product) => acc + parseFloat(product.value.replace("$", "")),
+        0
+      );
+      const outOfStock = updatedProducts.filter(
+        (product) => product.quantity === 0
+      ).length;
+      const categories = new Set(
+        updatedProducts.map((product) => product.category)
+      ).size;
 
-    dispatch(
-      setStats([
-        { label: 'Total Product', value: totalProducts },
-        { label: 'Total Store Value', value: `$${totalValue}` },
-        { label: 'Out of Stocks', value: outOfStock },
-        { label: 'No of Categories', value: categories },
-      ])
-    );
-  };
+      dispatch(
+        setStats([
+          { label: "Total Product", value: totalProducts },
+          { label: "Total Store Value", value: `$${totalValue}` },
+          { label: "Out of Stocks", value: outOfStock },
+          { label: "No of Categories", value: categories },
+        ])
+      );
+    },
+    [dispatch]
+  );
 
   useEffect(() => {
     fetchProducts();
